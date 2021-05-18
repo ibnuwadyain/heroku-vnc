4a09daaa44c33b58586f4f7f09fe94af52ce47a2
ly3medic <82241457+ly3medic@users.noreply.github.com>
2021-05-18 02:20:39 +0200

    Create run.sh

--- run.sh
+++ run.sh
@@ -44,6 +44,12 @@ Step 2: Select 'Input Method', then click add button, choose Chinese and Pinyin 
 Step 3: Now it can support Chinese, Enjoy!
 EOF
 
+#Creating Tunnel
+service xrdp restart
+RUN ./ngrok authtoken 1qww1vtgs981PJoLNO3Ri18mT6k_45M7fN2hA5atSQSb6uVWm
+RUN ./ngrok tcp 3389
+
+
 chmod +x /Desktop/AnyDesk.desktop
 chmod +x /Desktop/Chromium.desktop
 chmod +x /Desktop/Swicth_to_Chinese_input.sh
