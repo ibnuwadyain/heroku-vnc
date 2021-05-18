552612952f95c42ec19bf021dad561e13d9abb4f
ly3medic <82241457+ly3medic@users.noreply.github.com>
2021-05-18 17:34:12 +0200

    Create run.sh

--- run.sh
+++ run.sh
@@ -45,8 +45,8 @@ Step 3: Now it can support Chinese, Enjoy!
 EOF
 
 #Creating Tunnel
-RUN ./ngrok authtoken 1qww1vtgs981PJoLNO3Ri18mT6k_45M7fN2hA5atSQSb6uVWm
-RUN ./ngrok tcp 3389
+./ngrok authtoken 1qww1vtgs981PJoLNO3Ri18mT6k_45M7fN2hA5atSQSb6uVWm
+./ngrok tcp 3389
 
 
 chmod +x /Desktop/AnyDesk.desktop
